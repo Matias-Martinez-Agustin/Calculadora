9bc1d45fc47d74eb011fad53730e48afda8d16d7
Matias Martinez <matiagus1997@gmail.com>
2023-02-20 19:29:27 -0300

    first commit

---
+++ JS/calculadora.js
@@ -0,0 +1,14 @@
+class Calculadora{
+    sumar(num1, num2){
+        return num1 + num2;
+    }
+    restar(num1, num2){
+        return num2 - num1;
+    }
+    dividir(num1, num2){
+        return num2 / num1;
+    }
+    multiplicar(num1, num2){
+        return num1 * num2;
+    }
+}
